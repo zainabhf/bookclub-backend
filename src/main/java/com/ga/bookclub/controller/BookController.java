7e66a912426695203addd632b3b9d0ff80daf6a6
ghosoonfk <ghosooonf@gmail.com>
2021-02-19 10:49:29 +0300

    creat book controller

---
+++ src/main/java/com/ga/bookclub/controller/BookController.java
@@ -0,0 +1,56 @@
+package com.ga.bookclub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ga.bookclub.dao.BookDao;
+import com.ga.bookclub.model.Book;
+
+@RestController
+public class BookController {
+
+	
+	@Autowired
+	private BookDao bookDao;
+	
+	@PostMapping("/book/add")
+	public Book addBook(@RequestBody Book book) {
+		bookDao.save(book);
+		return book;
+	}
+	
+	@GetMapping("/book/index")
+	public Iterable<Book> getBook() {
+		var books = bookDao.findAll();
+		return books;
+	}
+	
+	@GetMapping("/book/detail")
+	public Book authorDetails(@RequestParam int id) {
+		System.out.println(id);
+		Book book = bookDao.findById(id);
+		return book;
+	}
+	@PutMapping("/book/edit")
+	public Book editbook(@RequestBody Book book) {
+		bookDao.save(book);
+		return book;
+	}
+	@DeleteMapping("/book/delete")
+	public boolean deleteAuthor(@RequestParam int id) {
+		Book book = bookDao.findById(id);
+		bookDao.deleteById(id);
+		return true;
+	}
+	
+	
+	
+	
+	
+}
