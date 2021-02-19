10a0ba7217c615f45140821719b91ca93f08be81
ghosoonfk <ghosooonf@gmail.com>
2021-02-19 08:48:33 +0300

    creat book model

--- src/main/java/com/ga/bookclub/model/Book.java
+++ src/main/java/com/ga/bookclub/model/Book.java
@@ -1,5 +1,30 @@
 package com.ga.bookclub.model;
 
-public class Book {
+import java.sql.Date;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Book")
+public class Book {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+
+	@Column(length = 6)
+	 private int numberOfpages  ;
+	private String authorName;
+	  private String  category ;
+	  
+	  
+	  @Column(length = 255)
+	  private String description;
+	  
+	  @Column(length = 255)
+	  private String image;
+	  private Date publish ;
+	  
+	 
 }
